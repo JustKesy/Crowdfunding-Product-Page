d2412a2259f8b4259584b83b3807c5d55b9648c1
kesy <92neco92@gmail.com>
2023-06-13 17:53:36 +0200

    Make Modal-window appear when click on 'Back this project' button, set up pledge section with it's features

--- script.js
+++ script.js
@@ -1,5 +1,10 @@
 const hamburgerMenu = document.querySelector(".hamburger-icon");
 const hamburgerMenuLinks = document.querySelector(".mobile-links");
+const BackUpBtn = document.querySelector(".title-btn");
+const modalWindow = document.querySelector(".modal-window");
+const closeModalBtn = document.querySelector(".close-btn");
+const radioBtns = document.querySelectorAll("input[type=radio]");
+const setPledgeWindows = Array.from(document.querySelectorAll(".set-pledge"));
 
 let isOpen = false;
 function openLinkMenu(e) {
@@ -13,4 +18,29 @@ function openLinkMenu(e) {
   }
 }
 
+function openClose() {
+  if (modalWindow.classList[1] == "hidden") {
+    modalWindow.classList.remove("hidden");
+  } else {
+    modalWindow.classList.add("hidden");
+  }
+}
+
 hamburgerMenu.addEventListener("click", openLinkMenu);
+
+BackUpBtn.addEventListener("click", openClose);
+
+closeModalBtn.addEventListener("click", openClose);
+
+function ifChecked(e) {
+  setPledgeWindows.forEach((window) =>
+    window.setAttribute("style", "display:none")
+  );
+  if (e.target.checked == true) {
+    let window = setPledgeWindows.find(
+      (x) => x.dataset.match == e.target.dataset.key
+    );
+    window.setAttribute("style", "display:flex");
+  }
+}
+radioBtns.forEach((btn) => btn.addEventListener("click", ifChecked));
