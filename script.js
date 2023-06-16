19c147dee1fd53299c67fe9cca967d854b39edfa
kesy <92neco92@gmail.com>
2023-06-16 15:19:23 +0200

    Make every count and status bar updates in pledge-infos section after any new pledge.

--- script.js
+++ script.js
@@ -8,12 +8,19 @@ const setPledgeWindows = Array.from(document.querySelectorAll(".set-pledge"));
 const pledges = Array.from(document.querySelectorAll(".pledge"));
 const modal = document.querySelector(".modal");
 const modalMsg = document.querySelector(".modal-msg");
-const continueBtns = document.querySelectorAll(".continue");
+const continueBtns = Array.from(document.querySelectorAll(".continue"));
 const modalMsgBtn = document.querySelector(".modal-msg-btn");
 const bookMarkPara = document.querySelector(".btn-par");
 const bookMarkBtn = document.querySelector(".title-bookmark");
 const bookMarkIcon = document.querySelector(".bookmark-icon");
-
+const moneyCount = document.querySelector(".money-count");
+const totalBackers = document.querySelector(".number-of-backers");
+const newPledgeInputs = Array.from(
+  document.querySelectorAll("input[type=tel]")
+);
+const statusBar = document.querySelector(".progress-bar");
+let curentMoneCount = "50000";
+let curentBakers = "4805";
 let isOpen = false;
 function openLinkMenu(e) {
   isOpen = !isOpen;
@@ -67,7 +74,59 @@ function ifChecked(e) {
 
 radioBtns.forEach((btn) => btn.addEventListener("click", ifChecked));
 
-function submitPledge() {
+function updateCurentMoneyCount(e) {
+  let x = newPledgeInputs.find(
+    (input) => e.target.dataset.index == input.dataset.key
+  );
+  curentMoneCount = (+curentMoneCount + +x.value).toString();
+}
+function updateMoneyCount(x) {
+  if (curentMoneCount.length <= 3) {
+    moneyCount.innerHTML = "$" + curentMoneCount;
+  } else if (curentMoneCount.length == 4) {
+    x = curentMoneCount.slice(0, 1) + "," + curentMoneCount.slice(1);
+    moneyCount.innerHTML = "$" + x;
+  } else if (curentMoneCount.length == 5) {
+    x = curentMoneCount.slice(0, 2) + "," + curentMoneCount.slice(2);
+    moneyCount.innerHTML = "$" + x;
+  } else {
+    moneyCount.innerHTML = "$" + "100,000";
+  }
+}
+function updateStatusBar(x) {
+  if (+curentMoneCount >= 100000) {
+    statusBar.setAttribute("style", "width:100%");
+  } else {
+    x = Math.ceil(+curentMoneCount / 1000);
+    statusBar.setAttribute("style", `width:${x}%`);
+  }
+}
+function updateTotalBackers(x) {
+  if (curentBakers.length <= 3) {
+    totalBackers.innerHTML = curentBakers;
+    curentBakers = (+curentBakers + 1).toString();
+  } else if (curentBakers.length == 4) {
+    totalBackers.innerHTML =
+      curentBakers.slice(0, 1) + "," + curentBakers.slice(1);
+    curentBakers = (+curentBakers + 1).toString();
+  } else if (curentBakers.length == 5) {
+    totalBackers.innerHTML =
+      curentBakers.slice(0, 2) + "," + curentBakers.slice(2);
+    curentBakers = (+curentBakers + 1).toString();
+  } else {
+    totalBackers.innerHTML =
+      curentBakers.slice(0, 3) + "," + curentBakers.slice(3);
+    curentBakers = (+curentBakers + 1).toString();
+  }
+}
+function submitPledge(e) {
+  updateCurentMoneyCount(e);
+  updateMoneyCount();
+  updateStatusBar();
+  updateTotalBackers();
+  if (+curentMoneCount > 100000) {
+    BackUpBtn.removeEventListener("click", openClose);
+  }
   modal.setAttribute("style", "display:none");
   modalMsg.setAttribute("style", "display:flex");
 }
@@ -76,7 +135,6 @@ continueBtns.forEach((btn) => btn.addEventListener("click", submitPledge));
 
 function closeModalMsg() {
   reset();
-  // modalWindow.setAttribute("style", "display:none");
   modalMsg.setAttribute("style", "display:none");
   openClose();
   radioBtns.forEach((btn) => (btn.checked = false));
@@ -97,5 +155,11 @@ function bookMark() {
     bookMarkIcon.setAttribute("style", "filter: contrast(0.5)");
   }
 }
+function start() {
+  updateMoneyCount();
+  updateStatusBar();
+  updateTotalBackers();
+}
 
 bookMarkBtn.addEventListener("click", bookMark);
+start();
